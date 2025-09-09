e1c8758a7f6bccb5d3bf74c1c338cd93e8c38fbf
Ernesto-Lora <98838975+Ernesto-Lora@users.noreply.github.com>
2025-09-09 14:40:59 -0600

    busqueda exhaustiva

---
+++ ejercicio.py
@@ -0,0 +1,32 @@
+def f(x):
+    return x**2 +54/x
+
+def condicion(f1,f2,f3):
+    result = f1 >= f2 and f2<=f3
+    print(f' vemos que {f1} >= {f2} <= {f3} es {result}')
+    return result
+
+def busqueda_exhaustiva(a,b,n,f):
+    delX = (b-a)/n
+    x1 = a
+    x2 = x1 + delX
+    x3 = x2 + delX
+    while(x3<=b):
+        print(f'Estamos en {x1}, {x2}, {x3}')
+        if condicion(f(x1),f(x2),f(x3)):
+            print('Ya encontramos el intervalo y es:')
+            print([x1,x3])
+            return [x1,x3]
+        else:
+            print('Hacemos otra iteracion')
+            x1 = x2
+            x2 = x3
+            x3 += delX
+    return ('no existe el minimo entre a,b o esta en los extremos')
+
+if __name__ == "__main__":
+    #lista = busqueda_exhaustiva(-10,5,10,f)
+    a = float(input('Ingrese "a"'))
+    b = float(input('Ingrese "b"'))
+    n = float(input('Ingrese "n"'))
+    lista = busqueda_exhaustiva(a,b,n,f)          
